f1ace0ee132b6b565594de68e7ec368c2d13b76a
rayan6ms <rayan6ms@gmail.com>
2026-04-08 17:06:04 -0300

    remove vite-tsconfig-paths dependency & some more unecessary files stuff

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	root: "src/renderer",
-	resolve: {
-		tsconfigPaths: true,
-	},
 	server: {
 		host: "127.0.0.1",
 		port: 5173,
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	resolve: {
-		tsconfigPaths: true,
-	},
 	test: {
 		environment: "node",
 		include: ["tests/**/*.test.ts"],
